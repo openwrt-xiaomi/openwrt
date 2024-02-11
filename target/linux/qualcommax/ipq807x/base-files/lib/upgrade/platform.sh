7d610f9aed6d642fc082b6d0fba7d2a332291546
Robert Marko <robimarko@gmail.com>
2024-02-11 18:57:52 +0100

    qualcommax: ipq807x: add Asus RT-AX89X
    Signed-off-by: Robert Marko <robimarko@gmail.com>

--- target/linux/qualcommax/ipq807x/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq807x/base-files/lib/upgrade/platform.sh
@@ -55,6 +55,11 @@ platform_do_upgrade() {
 	zbtlink,zbt-z800ax)
 		nand_do_upgrade "$1"
 		;;
+	asus,rt-ax89x)
+		CI_UBIPART="UBI_DEV"
+		CI_KERNPART="linux"
+		nand_do_upgrade "$1"
+		;;
 	buffalo,wxr-5950ax12)
 		CI_KERN_UBIPART="rootfs"
 		CI_ROOT_UBIPART="user_property"
